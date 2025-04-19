5bce08b0548f52f1ba5088d75db1d5b870674c3d
Devendar Singh Rawat <devendasinghrawat925@gmail.com>
2025-04-19 20:52:08 +0530

    new commit

--- Backend/src/routes/message.route.js
+++ Backend/src/routes/message.route.js
@@ -2,9 +2,10 @@ import express from 'express';
 import { protectRoute } from '../middleware/auth.middleware.js';
 import { getMessages, getUsersForSidebar, sendMessage } from '../controllers/message.controller.js';
 
-const router=express.Router();
-router.get("/users",protectRoute,getUsersForSidebar);
-router.get("/:id",protectRoute,getMessages);
-router.post("/send/:id",protectRoute,sendMessage);   //chats of mine with another user
+const router = express.Router();
+
+router.get("/users", protectRoute, getUsersForSidebar);         // ✅ specific
+router.post("/send/:id", protectRoute, sendMessage);            // ✅ specific
+router.get("/:id", protectRoute, getMessages);                  // ✅ least specific
 
 export default router;
